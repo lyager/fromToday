6dfca015ee612f10d67b31bc10b07a6e88ef4ee9
Jesper L. Nielsen <lyager@gmail.com>
2014-05-01 22:25:07 +0200

    First commit.

---
+++ fromToday.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+from datetime import datetime
+from dateutil.relativedelta import *
+#from dateutil.relativedelta import relativedelta
+
+def input(s):
+    m = re.match(r'(\d+) d', s)
+    if m:
+        dateAdd = relativedelta(days=int(m.group(1)))
+    m = re.match(r'(\d+) y', s)
+    if m:
+        dateAdd = relativedelta(years=int(m.group(1)))
+    m = re.match(r'^tod', s)
+    if m:
+        dateAdd = relativedelta(days=0)
+    m = re.match(r'^tom', s)
+    if m:
+        dateAdd = relativedelta(days=1)
+
+    # Weekdays
+    m = re.match(r'^mon', s)
+    if m:
+        dateAdd = relativedelta(weekday=MO)
+    m = re.match(r'^tue', s)
+    if m:
+        dateAdd = relativedelta(weekday=TU)
+    m = re.match(r'^wed', s)
+    if m:
+        dateAdd = relativedelta(weekday=WE)
+    m = re.match(r'^thu', s)
+    if m:
+        dateAdd = relativedelta(weekday=TH)
+    m = re.match(r'^fri', s)
+    if m:
+        dateAdd = relativedelta(weekday=FR)
+    m = re.match(r'^sat', s)
+    if m:
+        dateAdd = relativedelta(weekday=SA)
+    m = re.match(r'^sun', s)
+    if m:
+        dateAdd = relativedelta(weekday=SU)
+
+    # 
+    print datetime.today()+dateAdd
+
+
+
+input(sys.argv[1])
