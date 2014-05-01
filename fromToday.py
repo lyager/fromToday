ce4345b059adb664336a78b22f7f9e5570ef791b
Jesper L. Nielsen <lyager@gmail.com>
2014-05-01 22:47:07 +0200

    Needs serious rework to avoid non-existing Python Goto.

--- fromToday.py
+++ fromToday.py
@@ -7,6 +7,8 @@ from datetime import datetime
 from dateutil.relativedelta import *
 #from dateutil.relativedelta import relativedelta
 
+EXC_INT = Exception("Date interpretation")
+
 def input(s):
     m = re.match(r'(\d+) d', s)
     if m:
@@ -43,6 +45,34 @@ def input(s):
     m = re.match(r'^sun', s)
     if m:
         dateAdd = relativedelta(weekday=SU)
+
+    # Next Weekdays
+    m = re.match(r'^next ', s)
+    if m: 
+        dateAdd = relativedelta(days=7)
+        m = re.match(r'mon', s)
+        if m:
+            dateAdd += relativedelta(weekday=MO)
+        m = re.match(r'tue', s)
+        if m:
+            dateAdd = relativedelta(weekday=TU)
+        m = re.match(r'wed', s)
+        if m:
+            dateAdd = relativedelta(weekday=WE)
+        m = re.match(r'thu', s)
+        if m:
+            dateAdd = relativedelta(weekday=TH)
+        m = re.match(r'fri', s)
+        if m:
+            dateAdd = relativedelta(weekday=FR)
+        m = re.match(r'sat', s)
+        if m:
+            dateAdd = relativedelta(weekday=SA)
+        m = re.match(r'sun', s)
+        if m:
+            dateAdd = relativedelta(weekday=SU)
+        else:
+            raise EXC_INT
 
     # 
     print datetime.today()+dateAdd
